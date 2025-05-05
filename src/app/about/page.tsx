4d66519fe23cabda98f2ad38e72a09935151a47e
7linso <sfarmysf@gmail.com>
2025-05-05 09:21:40 -0500

    team template

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import team from '@/data/team.json'
 
 export default function AboutPage() {
     const bookingLink = 'https://laserbeauty-room-by-olga-rogozianska-101578.square.site/?fbclid=PAAaZES5tl2oNVz1WO5lwQ-Z8cOr4vxGlBirtF5a7a3iN9AI1DBkg_kqZmBVA'
@@ -89,6 +90,28 @@ export default function AboutPage() {
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+
+            <div className="max-w-5xl mx-auto px-4 py-4">
+                <h1 className="text-3xl font-bold text-teal-900 mb-8 border-b-2 border-teal-700 pb-2 shadow-[0_4px_2px_-2px_rgba(0,0,0,0.1)]">
+                    Meet Our Team
+                </h1>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {team.map((t, index) => (
+                        <div
+                            key={index}
+                            className="bg-white rounded-xl shadow-md p-5 hover:shadow-xl border-3 border-teal-700 transition-shadow duration-300"
+                        >
+                            <img
+                                src={t.image}
+                                alt={t.name}
+                                className="w-full h-48 object-cover rounded-lg mb-4"
+                            />
+                            <h2 className="text-xl font-semibold text-teal-800 mb-2">{t.name}</h2>
+                            <p className="text-gray-600">{t.description}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
