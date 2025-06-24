9631dfbf770cda6b630c3ba05305c75188dfd0fa
7linso <sfarmysf@gmail.com>
2025-06-24 09:20:51 -0500

    link fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,12 +4,12 @@ export default function NotFoundPage() {
     return (<>
         <div className="text-teal-900 flex items-center justify-center min-h-screen text-center">
             <div>
-                <p className="text-2xl font-semibold mb-4">
+                <h1 className="text-2xl font-semibold mb-4">
                     Sorry, you are searching for <br /> the page that does not exist
-                </p>
-                <a href="/" className="text-lg hover:text-teal-700 transition underline">
+                </h1>
+                <Link href="/" className="text-lg hover:text-teal-700 transition underline">
                     Go Back to Home page
-                </a>
+                </Link>
             </div>
         </div>
     </>)
