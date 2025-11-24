51a62fbbf997dc63b3f5d4eb580ca34de24b144c
linso <sfarmysf@gmail.com>
2025-11-24 09:59:37 -0600

    for static build

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/dqvthdtuu/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/dqvthdtuu/**",
       },
     ],
-  }
+  },
 };
 
 export default nextConfig;
